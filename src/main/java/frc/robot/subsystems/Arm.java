606f62ec3cfe7bd3d77bb1dade950913b456865a
Mihai Nicoara <nicoaram@s.dcsdk12.org>
2023-01-28 18:48:53 -0700

    fuck

---
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.Ports;
+
+public class Arm extends SubsystemBase {
+  /** Creates a new Claw. */
+
+  // Initializes the motors
+  final static WPI_TalonSRX leftMotor = new WPI_TalonSRX(2);
+  final static WPI_TalonSRX rightMotor = new WPI_TalonSRX(31);
+
+
+  public Arm() {
+    leftMotor.configFactoryDefault();
+    rightMotor.configFactoryDefault();
+
+
+    // Sets a motor as inverted
+    leftMotor.setInverted(true);
+  }
+
+  public void closeClaw(Number percent) {
+    leftMotor.set(ControlMode.PercentOutput, (double)percent);
+    rightMotor.set(ControlMode.PercentOutput, (double) percent);
+  }
+
+  @Override
+  public void periodic() {
+
+  }
+}
